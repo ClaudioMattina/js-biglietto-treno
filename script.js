77ae6833412e46dbb77f751c4afe4c07616550e9
CLaudioBruni <claudio.bruni95@hotmail.com>
2022-02-22 16:39:11 +0100

    percentuale minorenni

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-
+const km = prompt("inserisci km");
+console.log(km);
 
 const age = prompt("Inserisci età");
 console.log(age);
 
-const km = prompt("inserisci km");
-console.log(km);
+const priceKm = 0.21;
 
 let somma = km * 0.21;
 console.log(somma);
+
+
+
+if (age <= 17 ) {
+    somma = ((somma * 20) / 100) ;
+} else { somma = somma };
+console.log(somma)
+
+
